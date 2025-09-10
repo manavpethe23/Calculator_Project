981a8e2bf6739363d6ee0c93a0b2279efe066a27
ManavPethe <146188462+manavpethe23@users.noreply.github.com>
2025-09-10 14:42:20 +0530

    Update Demo.java

--- Calculator/src/com/Demo/Demo.java
+++ Calculator/src/com/Demo/Demo.java
@@ -2,8 +2,14 @@ package com.Demo;
 
 public class Demo {
 
+	public void add() {
+		System.out.println(45+62);
+	}
+	
 	public static void main(String[] args) {
-		
+		Demo d = new Demo();
+			d.add();
 	}
 
 }
+
